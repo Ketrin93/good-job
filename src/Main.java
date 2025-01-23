7144eace35b804134f3afcad043fb69989be6348
hello <lazareva_ekaterina_96_96@mail.ru>
2025-01-23 18:12:09 +0300

    ДЗ2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
-    public static void main(String[] args){System.out.println("Task 1");
+    public static void main(String[] args) {
+        System.out.println("Task 1");
         for (int i = 1; i <= 10; i = i + 1) {
             System.out.println("Итерация цикла " + i);
         }
@@ -34,6 +35,7 @@ public class Main {
             total = total + salary;
             System.out.println("Месяц" + (i + 1) + " сумма накоплений равна" + total + "рублей");
         }
+
         System.out.println(" Task 9");
         for (int i = 0; i < 12; i++) {
             total = total + total / 100;
@@ -47,8 +49,94 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println("2 * " + i + " = " + 2 * i);
         }
+        System.out.println("Задача 1");
+        int deposit = 15000;
+        int summ = 0;
+        int day = 0;
+        while (summ < 2459000) {
+            summ = summ + deposit;
+            day++;
+            System.out.println("день " + day + " Итого " + summ);
+        }
+        System.out.println("Задача 2");
+        int start = 1;
+        while (start < 10) {
+            System.out.print("1 2 3 4 5 6 7 8 9 10 ");
+            start = start + 1;
+        }
+        System.out.println();
+        for (int i = 10; i > 1; i--) {
+            System.out.print(" 10 9 8 7 6 5 4 3 2 1");
+
+
+        }
+        System.out.println("Задача 3");
+
+        int population = 12_000_000;
+        int birthRatePer1000 = 17;
+        int mortalPer1000 = 8;
+        int yearNow = 2024;
+        for (int year = yearNow; year < yearNow + 10; year++) {
+            population += population / 1000 * birthRatePer1000 - population / 1000 * mortalPer1000;
+            System.out.println("Год " + year + ", численность населения составляет " + population);
+        }
+        System.out.println("Задача 4");
+        int deposit2 = 15000;
+        double monthlyPercent = 0.07;
+        int totalAmount = 12_000_000;
+        int numberMonth = 1;
+
+        while (deposit <= totalAmount) {
+            deposit += deposit2 * monthlyPercent;
+            System.out.printf("Месяц %d, сумма накоплений: %d%n", numberMonth, deposit);
+            numberMonth++;
+        }
+
+
+        System.out.println("Задача 5");
+        int vasyaPut = 15000;
+        int zlato = 0;
+        while (vasyaPut < 12000000) {
+            vasyaPut = vasyaPut + vasyaPut / 100 * 7;
+            zlato = zlato + 1;
+            if (zlato % 6 == 0) {
+            }
+            System.out.println("Месяц" + zlato + "вклад равен" + vasyaPut);
+        }
+        System.out.println("Задача 6");
+
+        int sum = 15000;
+        int year = 9;
+        int monthInTheYear = 12;
+        int month2 = 0;
+        int totalMonth = year * monthInTheYear;
+        while (total  <= totalMonth)
+            total = total + sum;
+        total = total + (total * 7) / 100;
+        if (month2 % 6 == 0) {
+            System.out.println(" Месяц" + month2 + "сумма накоплений равна" + total);
+        }
+        System.out.println("Задача 7");
+        int friday = 1;
+        while (friday <= 31) {
+            System.out.println("Сегодня пятница, " + friday + " -е число.Необходимо подготовить отчет");
+            friday += 7;
+        }
+        System.out.println(" Задание 8");
+        int yearBefore = 2024 - 200;
+        int yearAfter = 2024 + 100;
+
+        for (year = yearBefore; year < yearAfter; year ++) {
+            if (year % 79 == 0){
+                System.out.println(year);}
+        }
     }
-
-
-
 }
+
+
+
+
+
+
+
+
